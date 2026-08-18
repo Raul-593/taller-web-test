85fae90b4c695874a1b984bb9bcd94a01a1153cc
Raul Viteri <raulviterip@gmail.com>
2026-08-18 17:46:28 -0500

    agregar pipeline CI

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from pages.login_page import LoginPage
 
 base_url = "http://localhost:3000"
@@ -11,6 +14,11 @@ test_password = "`QaTest123!`"
 @pytest.fixture
 def driver():
     """Abre y cierra el navegador para cada test"""
+    options = Options()
+    if os.getenv("SELENIUM_HEADLESS") == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
     service = Service(ChromeDriverManager().install())
     d = webdriver.Chrome(service=service)
     d.implicitly_wait(5)
